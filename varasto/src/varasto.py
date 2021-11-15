b654c48343a5cf5ac9d94b93ebda7be6e9d61647
ferealqq <pekka.mattinen@outlook.com>
2021-11-15 15:41:33 +0200

    --amend

--- varasto/src/varasto.py
+++ varasto/src/varasto.py
@@ -1,11 +1,7 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
-        
+        self.tilavuus = tilavuus if tilavuus > 0.0 else 0.0
+
         if alku_saldo < 0.0:
             # virheellinen, nollataan
             self.saldo = 0.0
@@ -31,107 +27,15 @@ class Varasto:
     def ota_varastosta(self, maara):
         if maara < 0:
             return 0.0
-        if maara > self.saldo: 
-            kaiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiikkimitävoidaan = self.saldo
+        if maara > self.saldo:
+            kaikki = self.saldo
             self.saldo = 0.0
 
-            return kaiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiikkimitävoidaan
+            return kaikki
 
         self.saldo = self.saldo - maara
 
         return maara
-
-    def nested_blocks(self,one, two ,three, four, five, six,seven, eigth, nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen, twenty):
-        lst = range(100)
-        for i in lst:
-            for j in lst:
-                for k in lst:
-                    print(f"{i} {j} {k}")
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        useless_line = True
-        # twenty useless lines of code
-        # twenty useless lines of code
-        # twenty useless lines of code
-        # twenty useless lines of code
-        # twenty useless lines of code
-        # twenty useless lines of code
-        # twenty useless lines of code
-        # twenty useless lines of code
-        # twenty useless lines of code
-        # twenty useless lines of code
-        # twenty useless lines of code
-        # twenty useless lines of code
-        # twenty useless lines of code
-        # twenty useless lines of code
-        # twenty useless lines of code
-        # twenty useless lines of code
-        # twenty useless lines of code
-        # twenty useless lines of code
-        # twenty useless lines of code
-        # twenty useless lines of code
-        # twenty useless lines of code
-        # twenty useless lines of code
-
-        return "nested_blocks()"            
-            
 
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
